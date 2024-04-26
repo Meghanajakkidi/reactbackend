40a008e1ecef9e05f83983af32f497bd32bd49cc
meghana <meghanabscmpcs@gmail.com>
2024-04-26 16:50:48 +0530

    my code

---
+++ routes/Item.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const item = ["hello world"]
+
+
+router.get("/list",function(req,res){
+    res.send(item)
+})
+
+module.exports=router
